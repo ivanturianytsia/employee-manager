1c4d052534ac6e32681ff06bf6764be68c74978f
ivanturianytsia <vanyaturianitsa@gmail.com>
2018-01-06 15:44:19 +0100

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDArj7IGFTfFmWxTy-3BvYxG2sUyN9wLvc",
+  authDomain: "employees-3cce8.firebaseapp.com",
+  databaseURL: "https://employees-3cce8.firebaseio.com",
+  projectId: "employees-3cce8",
+  storageBucket: "employees-3cce8.appspot.com",
+  messagingSenderId: "421552445234"
+}
